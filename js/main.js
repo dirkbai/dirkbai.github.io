a86071cbac4bd9d1c84ff184c8a7b6baf6c07b1f
Dirk B <dirk.baigent@example.com>
2017-09-08 20:41:12 +0200

    nav overlay

---
+++ js/main.js
@@ -0,0 +1,47 @@
+var mn = $(".main-nav");
+    mns = "main-nav-scrolled";
+    hdr = $('header').height();
+
+var myNav = document.getElementById("myNav");
+var overlyStart = document.getElementById("overlay-nav");
+
+var mainNav = document.getElementById("main-nav");
+
+// $('window').scroll(function(){
+//  if( $(this).scrollTop() > 50 ) {
+//    mn.addClass("main-nav-scrolled");
+//  } else {
+//    mn.removeClass("main-nav-scrolled");
+//  }
+// });
+
+document.addEventListener('scroll', function() {
+ if( $(this).scrollTop() > hdr ) {
+   mn.addClass(mns);
+  myNav.className = "overlay nav_height_top";
+ } else {
+   mn.removeClass(mns);
+   myNav.className = "overlay";
+ }
+});
+
+
+//---------------------------------------- Nav Overlay
+
+// if (mainNav.className === "main-nav") {
+//   myNav.className = "overlay nav_height_btm";
+// } else if(mainNav.className === "main-nav main-nav-scrolled") {
+//  myNav.className = "overlay nav_height_top";
+// }
+
+/* Open */
+overlyStart.addEventListener('click', function () {
+     document.getElementById("myNav").style.height = "100%";
+ });
+
+
+
+/* Close */
+myNav.addEventListener('click', function () {
+    document.getElementById("myNav").style.height = "0%";
+});
