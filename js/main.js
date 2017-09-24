0fc8129d1f8ce5fa81e45c2567bb4ca00722be3b
Dirk B <dirk.baigent@example.com>
2017-09-24 13:26:14 +0200

    Skills

--- js/main.js
+++ js/main.js
@@ -320,42 +320,42 @@ for (var i = 0; i < funInactive.length; i++) {
 
 Chart.defaults.global.legend.display = false;
 
-var ctx = document.getElementById("myChart").getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'horizontalBar',
-    data: {
-        labels: ["Red", "Blue", "Yellow"],
-        datasets: [{
-            data: [12, 19, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)'
-            ],
-            borderWidth: 0
-
-        }]
-    },
-    options: {
-      scales: {
-          xAxes: [{
-              gridLines: {
-                  display: false
-              }
-          }],
-          yAxes: [{
-              gridLines: {
-                  display: false
-              }
-          }]
-      }
-    }
-});
+// var ctx = document.getElementById("myChart").getContext('2d');
+// var myChart = new Chart(ctx, {
+//     type: 'horizontalBar',
+//     data: {
+//         labels: ["Red", "Blue", "Yellow"],
+//         datasets: [{
+//             data: [12, 19, 3],
+//             backgroundColor: [
+//                 'rgba(255, 99, 132, 0.2)',
+//                 'rgba(54, 162, 235, 0.2)',
+//                 'rgba(255, 206, 86, 0.2)'
+//             ],
+//             borderColor: [
+//                 'rgba(255,99,132,1)',
+//                 'rgba(54, 162, 235, 1)',
+//                 'rgba(255, 206, 86, 1)'
+//             ],
+//             borderWidth: 0
+//
+//         }]
+//     },
+//     options: {
+//       scales: {
+//           xAxes: [{
+//               gridLines: {
+//                   display: false
+//               }
+//           }],
+//           yAxes: [{
+//               gridLines: {
+//                   display: false
+//               }
+//           }]
+//       }
+//     }
+// });
 
 
 
