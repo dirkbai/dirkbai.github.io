285f089a66c36024b13e08fa0f4f21b0aa952b1c
Dirk B <dirk.baigent@example.com>
2017-10-02 16:12:23 +0200

    gulp start

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,26 @@
 'use strict';
+
+var gulp = require('gulp');
+var image = require('gulp-image');
+var sass = require('gulp-sass');
+
+gulp.task('image', function () {
+  gulp.src('./img/Fun/*')
+    .pipe(image({
+      pngquant: true,
+      optipng: false,
+      zopfliping: true,
+      jpegRecompress: true,
+      jpegoptim: false,
+      mozjpeg: false,
+      gifsicle: true,
+      svgo: true
+    }))
+    .pipe(gulp.dest('./img/Fun/*'));
+});
+
+gulp.task('compileSass', function(){
+  gulp.src("scss/style.scss")
+  .pipe(sass())
+  .pipe(gulp.dest('css'))
+})
