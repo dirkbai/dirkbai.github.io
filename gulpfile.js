bad83598347dd31c1b509a85c9b96bf7376f6e93
Dirk B <dirk.baigent@example.com>
2017-10-03 19:36:04 +0200

    process-css

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,10 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var postcssZindex = require('postcss-zindex');
+var concat = require('gulp-concat');
+var cleanCSS = require('gulp-clean-css');
 
 gulp.task('compileSass', function(){
   gulp.src("scss/style.scss")
@@ -14,9 +18,9 @@ gulp.task('process-css', function() {
   var plugins = [
     autoprefixer({
       browsers: ['last 3 version']
-    }),
-    postcssZindex({startIndex: 1})
-  ];
+    })
+  //   postcssZindex({startIndex: 1})
+  // ];
   return gulp.src('./css/*.css')
     .pipe(postcss(plugins))
     .pipe(cleanCSS({
