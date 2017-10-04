7aa3e238673f2249fdb57a4813ba85a2dbe9719d
Dirk B <dirk.baigent@example.com>
2017-10-04 06:45:44 +0200

    lazyload

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,8 @@ var autoprefixer = require('autoprefixer');
 var postcssZindex = require('postcss-zindex');
 var concat = require('gulp-concat');
 var cleanCSS = require('gulp-clean-css');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
 
 gulp.task('compileSass', function(){
   gulp.src("scss/style.scss")
@@ -32,3 +34,33 @@ gulp.task('process-css', function() {
     .pipe(concat('prod.min.css'))
     .pipe(gulp.dest('css'));
 });
+
+
+gulp.task("concatScripts", function(){
+  return gulp.src([
+            'js/jquery-3.2.1.min.js',
+            'js/slick.min.js',
+            'js/noframework.waypoints.min.js',
+            'js/waypoints.debug.js',
+            'js/lazyload.min.js',
+            'js/main.js'])
+      .pipe(concat("app.js"))
+      .pipe(gulp.dest("js"));
+})
+
+gulp.task("minifyScripts", ["concatScripts"], function(){
+  return gulp.src("js/app.js")
+    .pipe(uglify())
+    .pipe(rename('app.min.js'))
+    .pipe(gulp.dest('js'))
+});
+
+gulp.task('watchFiles', function(){
+  gulp.watch('css/*.css', ['compileSass']);
+  gulp.watch('css/*.css', ['process-css']);
+  gulp.watch('js/*.js', ['concatScripts']);
+})
+
+gulp.task('serve', ['watchFiles']);
+
+gulp.task('default',['compileSass','process-css','minifyScripts']);
